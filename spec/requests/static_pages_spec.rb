7ac8a6b1d04647fe02907327fc85f2a1e3efba0d
Kennedy Ramirez <kramirez@chicagotechacademy.org>
2014-04-08 15:02:28 -0500

    Added some code

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,35 +4,52 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the content Sample App" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
-      page.should have_selector("h1", :text =>'Sample App')
+      visit root_path
+      page.should have_selector("h1", text: 'Sample App')
     end
     it "should have a title" do
-      visit '/static_pages/home'
-      page.should have_selector("title", :text =>'Home Page')
+      visit root_path
+      page.should have_selector("title", text: 'Home Page')
     end
   end
+  
   describe "Help page" do
     it "should have the content Help Page" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/help'
-      page.should have_selector("h1", :text =>'Help Page')
+      visit help_path
+      page.should have_selector("h1", text: 'Help Page')
     end
    it "should have a title" do
-      visit '/static_pages/help'
-      page.should have_selector("title", :text =>'Help Page')
+      visit help_path
+      page.should have_selector("title", text: 'Help Page')
     end
   end
+  
     describe "About Us page" do
     it "should have the content About Us" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/about'
-      page.should have_selector("h1", :text => 'About Us')
+      visit about_path
+      page.should have_selector("h1", text: 'About Us')
     end
      it "should have a title" do
-      visit '/static_pages/about'
-      page.should have_selector("title", :text =>'About Page')
+      visit about_path
+      page.should have_selector("title", text: 'About Page')
     end
+
+    describe "Contact page" do
+    it "should have the content Contact Us" do
+    visit contact_path
+    pages.should have_selector('h1',text: 'Contact Us')
+    end
+    it "should have a title" do
+    visit contact_path
+    pages.should have have_selector('title', text:'Contact Page')
+
   end
 end
+
+end
+
+
+
 
