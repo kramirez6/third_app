84f79e929e2a14b854d33ca366e50903fd146435
Kennedy Ramirez <kramirez@chicagotechacademy.org>
2014-04-11 12:00:41 -0500

    Finished layout routes and associated test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -23,7 +23,7 @@ describe "Help page" do
     before {visit about_path}
 
     it {should have_selector("h1", text: 'About Us')}
-    it {should have_selector("title", text: 'About Page')}
+    it {should have_selector("title", text: 'About Us')}
   
 end
   describe "Contact page" do
@@ -33,7 +33,23 @@ end
    it {should have_selector('title', text:'Contact Page')}
 
   end
+  it "should have the correct links" do
+  visit root_path 
+  
+  click_link "About"
+  page.should have_selector "title", text: 'About Us' 
+  
+   click_link "Help"
+  page.should have_selector "title", text: 'Help'
 
+   click_link "Contact"
+  page.should have_selector "title", text: 'Contact'
+  
+  click_link "Home"
+  
+  click_link "Sign up now!"
+  page.should have_selector "title", text: 'Sign up'
+  end
 end
 
 
