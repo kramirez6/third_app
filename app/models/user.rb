3dccc25c72b497e90a7a7c85e18517e5420a2090
Kennedy Ramirez <kramirez@chicagotechacademy.org>
2014-04-16 15:00:38 -0500

    Added some code

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 30}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+  	uniqueness: { case_sensetive: false }
+
+
 end
